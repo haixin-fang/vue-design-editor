2e1f738f397c4792e79946398144282f2601b490
haixin <haixin.fang@ly.com>
2022-08-27 18:47:14 +0800

    fix: 基础路径错误

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), crossOriginIsolation()]
+  plugins: [vue(), crossOriginIsolation()],
+  base: '/fabricjs_vue'
 })
