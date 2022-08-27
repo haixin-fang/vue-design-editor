976dd86a20ff15193f5bf6b9fe6a6f1aa9487acb
haixin <haixin.fang@ly.com>
2022-08-27 18:37:56 +0800

    build: 工作流构建失败

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'commitlint.config.js'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
