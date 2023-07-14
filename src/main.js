69fdc098a2aa7fbfcfd4f4f89b974c2dc18a8df8
haixin.fang <1875934025@qq.com>
2023-07-14 16:06:09 +0800

    feat: 正式开启图片编辑器之旅

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import "./gifjs/gif";
-const app = createApp(App);
-app.use(ElementPlus);
-app.mount("#app");
