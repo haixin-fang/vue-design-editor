8ecb5ca70908bd4e14770110b119b85d40f3eeba
haixin.fang <1875934025@qq.com>
2023-10-31 11:21:31 +0800

    fix: 注释

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,23 +1,13 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
-// import store from '../store'
-// import { getToken } from '@/utils/auth'
 
-// create an axios instance
 const service = axios.create({
-  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 });
 
 // 添加请求拦截器
 axios.interceptors.request.use(
   function (config) {
-    // 在发送请求之前做些什么
-    //
-    // if (config.params.operateItemList) {
-
-    // }
     return config;
   },
   function (error) {
